993b28d79d1ae675c21f5c9287414ef4bf241c35
Siddharth Dhanasekar <63680554+SiddharthDhanasekar@users.noreply.github.com>
2025-07-31 22:45:41 -0400

    new hard traps added

--- app/services/ai_engine/update/evaluate_document_prufia_abbreviated_only.py
+++ app/services/ai_engine/update/evaluate_document_prufia_abbreviated_only.py
@@ -19,6 +19,60 @@ def matches_hard_traps(doc):
             doc.get("TT", 0) > 49.99 and
             doc.get("PGFI", 0) > 8 and
             (doc.get("SF", 100) < 45 or doc.get("SM", 100) < 52)
+        ) or
+        (
+            11 <= doc.get("PGFI", 0) <= 17 and
+            doc.get("PF", 0) >= 2.0 and
+            doc.get("SyntheticChaos", 100) <= 75 and
+            doc.get("SM", 0) <= 54 and
+            doc.get("TT", 0) >= 45
+        ) or
+        (
+            doc.get("PF", 0) >= 2.25 and
+            doc.get("SM", 0) < 58 and
+            doc.get("SF", 0) < 43 and
+            doc.get("SyntheticChaos", 100) < 80 and
+            doc.get("PGFI", 0) < 17
+        ) or
+        (
+            doc.get("PF", 0) > 1.85 and
+            doc.get("SM", 0) < 58 and
+            doc.get("SF", 0) < 48 and
+            doc.get("SyntheticChaos", 100) < 80 and
+            doc.get("PGFI", 0) < 17.5 and
+            doc.get("TT", 0) > 45
+        ) or
+        (
+            doc.get("MC", 0) >= 63 and
+            doc.get("SM", 0) <= 56 and
+            doc.get("SyntheticChaos", 100) <= 75 and
+            doc.get("PF", 0) >= 2.1
+        ) or
+        (
+            doc.get("PGFI", 0) < 17.5 and
+            doc.get("PF", 0) > 2.1 and
+            doc.get("TT", 0) > 46.5 and
+            doc.get("SM", 0) < 52 and
+            doc.get("SyntheticChaos", 100) < 78
+        ) or
+        (
+            doc.get("PF", 0) > 2.2 and
+            doc.get("StructureConsistency", 0) > 25 and
+            doc.get("SyntheticChaos", 100) < 75 and
+            10 <= doc.get("PGFI", 0) <= 18 and
+            doc.get("SM", 0) < 53
+        ) or
+        (
+            doc.get("PF", 0) > 2.15 and
+            doc.get("SyntheticChaos", 100) < 60 and
+            doc.get("SF", 0) < 42 and
+            doc.get("SM", 0) < 55
+        ) or
+        (
+            doc.get("SyntheticChaos", 100) >= 80 and
+            doc.get("StructureConsistency", 0) < 25 and
+            doc.get("PGFI", 0) < 19 and
+            doc.get("PF", 0) > 2
         )
     )
 
